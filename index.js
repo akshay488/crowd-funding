edf505903ed445e704f920edb758bc0284e59ac9
akshay488 <>
2021-05-22 11:33:21 +0530

    complete i guess

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-
+// nav menu function
 
 function openPages(ele){
   var x = ele.id;
+    let element = document.querySelector('.myNav');
 switch (x) {
   case 'form':   document.getElementById("myForm").style.display = "block";
 
     break;
+ case 'openMenu': element.classList.remove('close');
+                  element.classList.add('open');
 
-    case 'openMenu': document.getElementById('myNav').style.display = "block";
+   break;
 
-      break;
       }
 
     }
 
 function closePages(ele){
   var x = ele.id;
+  let element = document.querySelector('.myNav');
 switch (x) {
-case 'got_it':
-              document.getElementById('thanks').style.display = "none";
+case 'got_it':        document.getElementById('thanks').style.display = "none";
 
   break;
-case 'closeMenu':  document.getElementById('myNav').style.display = "none";
-                  
-  break;
 
+  case 'closeMenu':element.classList.remove('open');
+                   element.classList.add('close');
+    break;
     }
 }
